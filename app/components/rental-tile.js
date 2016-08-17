dbadfaba24364c07abc8ab10aded487908d9d7db
Kevin VanEvery <kvanever@gmail.com>
2016-08-17 14:28:43 -0700

    add dynamic routing and refactor details into a new route

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isImageShowing: false,
-  isUpdateFormClosed: true,
   actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
@@ -10,13 +9,5 @@ export default Ember.Component.extend({
     imageHide: function() {
       this.set('isImageShowing', false);
     },
-    update(rental, params) {
-      this.sendAction('update', rental, params);
-    },
-    delete(rental) {
-      if(confirm('Are you sure you want to delete this rental?')) {
-        this.sendAction('destroyRental', rental);
-      }
-    }
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  updateRentalForm: false,
   model() {
     return Ember.RSVP.hash({
       rentals: this.store.findAll('rental'),
@@ -14,10 +13,6 @@ export default Ember.Route.extend({
     Ember.set(controller, 'announcements', model.announcements);
   },
   actions: {
-    destroyRental(rental) {
-      rental.destroyRecord();
-      this.transitionTo('index');
-    },
     destroyAnnouncement(announcement) {
       announcement.destroyRecord();
       this.transitionTo('index');
@@ -25,15 +20,6 @@ export default Ember.Route.extend({
     save3(params) {
       var newRental = this.store.createRecord('rental',params);
       newRental.save();
-      this.transitionTo('index');
-    },
-    update(rental, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          rental.set(key,params[key]);
-        }
-      });
-      rental.save();
       this.transitionTo('index');
     },
     saveNewAnnouncement(params) {
